3dc303838c2a2b772f22e4a3e102b93e865af52f
misvu <anvu4577@student.su.se>
2017-11-05 19:24:03 +0100

    code refactoring

--- multi-path-stories-app/multi-path-stories-app.py
+++ multi-path-stories-app/multi-path-stories-app.py
@@ -1,30 +1,28 @@
 from flask import Flask, request, render_template, redirect
 
-def get_sentence_for_story(story):
-  if(story != None):
-    return dictOfStories[story]["mainSentence"]
+def get_sentence_for_story(story_id):
+  if(story_id != None):
+    return dict_of_stories[story_id]["main_sentence"]
 
 def get_next_story_id():
-    return ("a" + str(len(dictOfStories) + 1))
+    return ("a" + str(len(dict_of_stories) + 1))
 
 def create_new_story_dict(sentence):
-  return {"mainSentence": sentence, "top": None, "right": None, "bottom": None, "left": None }
+  return {"main_sentence": sentence, "top": None, "right": None, "bottom": None, "left": None }
 
-def link_story_to_another(currentStory, position, nextStory):
-  dictOfStories[currentStory][position] = nextStory
+def link_story_to_another(current_story_id, position, next_story_id):
+  dict_of_stories[current_story_id][position] = next_story_id
 
-def add_story(currentStory, position, sentence):
-  story_ID = get_next_story_id()
+def add_story(current_story_id, position, sentence):
+  story_id = get_next_story_id()
   new_story_dict = create_new_story_dict(sentence)
-  dictOfStories[story_ID] = new_story_dict
-  link_story_to_another(currentStory, position, story_ID)
-  return story_ID
+  dict_of_stories[story_id] = new_story_dict
+  link_story_to_another(current_story_id, position, story_id)
+  return story_id
 
-dictOfStories = {}
-dictOfStories["a1"] = create_new_story_dict("Once upon a time there was a girl who wanted to fly.")
-add_story("a1", "top", "A wolf ate her")
-add_story("a1", "right", "And she also wanted to have pizza.")
-add_story("a2", "top", "And she died.")
+dict_of_stories = {}
+dict_of_stories["a1"] = create_new_story_dict("Once upon a time there was a girl who wanted to fly.")
+
 
 app = Flask(__name__, static_folder="static", static_path="")
 
@@ -35,31 +33,31 @@ def index():
 @app.route('/new_story', methods=['POST'])
 def new_story():
     position = request.form["position"]
-    currentStory = request.form["currentStory"]
-    newSentence = request.form["newSentence"]
-    story_id = add_story(currentStory, position, newSentence)
+    current_story_id = request.form["current_story_id"]
+    new_sentence = request.form["new_sentence"]
+    story_id = add_story(current_story_id, position, new_sentence)
     return redirect("/story/" + story_id)
 
-@app.route('/story/<story>')
-def show_story(story):
+@app.route('/story/<story_id>')
+def show_story(story_id):
 
-    currentStory = dictOfStories.get(story)
+    current_story_dict = dict_of_stories.get(story_id)
 
-    topSentence = get_sentence_for_story(currentStory["top"])
-    rightSentence = get_sentence_for_story(currentStory["right"])
-    bottomSentence = get_sentence_for_story(currentStory["bottom"])
-    leftSentence = get_sentence_for_story(currentStory["left"])
+    top_sentence = get_sentence_for_story(current_story_dict["top"])
+    right_sentence = get_sentence_for_story(current_story_dict["right"])
+    bottom_sentence = get_sentence_for_story(current_story_dict["bottom"])
+    left_sentence = get_sentence_for_story(current_story_dict["left"])
 
-    return render_template("index.html", mainSentence=currentStory["mainSentence"],
-                           topLink=currentStory["top"],
-                           bottomLink=currentStory["bottom"],
-                           leftLink=currentStory["left"],
-                           rightLink=currentStory["right"],
-                           topSentence=topSentence,
-                           rightSentence=rightSentence,
-                           bottomSentence=bottomSentence,
-                           leftSentence=leftSentence,
-                           currentStory = story)
+    return render_template("index.html", main_sentence=current_story_dict["main_sentence"],
+                           top_story_id=current_story_dict["top"],
+                           bottom_story_id=current_story_dict["bottom"],
+                           left_story_id=current_story_dict["left"],
+                           right_story_id=current_story_dict["right"],
+                           top_sentence=top_sentence,
+                           right_sentence=right_sentence,
+                           bottom_sentence=bottom_sentence,
+                           left_sentence=left_sentence,
+                           current_story_id = story_id)
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
